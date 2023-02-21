0a3401fa5d90990bf95ae427da64f2130440933d
IanWGotcher <112666105+IanWGotcher@users.noreply.github.com>
2023-02-21 16:45:07 -0700

    Added the memory class in memory.py

---
+++ memory.py
@@ -0,0 +1,10 @@
+class memory:
+
+    def __init__(self):
+        self.memory = [0] * 100
+
+    def __setitem__(self, index, value):
+        self.memory[index] = value
+    
+    def __getitem__(self, index):
+        return self.memory[index]
