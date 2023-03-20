b82416bdbce2ee4b2477489c58d0a5b85151575b
Rowley <54674066+drowley8@users.noreply.github.com>
2023-03-20 13:53:07 -0600

    make Memory iterable

--- memory.py
+++ memory.py
@@ -1,6 +1,7 @@
 class Memory:
 
     def __init__(self):
+        self.i = -1
         self.memory = [0] * 100
 
     def __setitem__(self, index, value):
@@ -8,3 +9,13 @@ class Memory:
     
     def __getitem__(self, index):
         return self.memory[index]
+    
+    def __iter__(self):
+        self.i = -1
+        return self
+    
+    def __next__(self):
+        self.i += 1
+        if self.i < 100:
+            return self[self.i]
+        raise StopIteration
