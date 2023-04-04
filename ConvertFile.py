060e08bb28096d26cd330247aab057a5fd93d6ab
IanWGotcher <112666105+IanWGotcher@users.noreply.github.com>
2023-04-04 14:05:48 -0600

    Completed, uses Parser Class, fixed bugs
    Now uses the Parser class and not parse
    fixed bugs and now works correctly

--- ConvertFile.py
+++ ConvertFile.py
@@ -1,43 +1,45 @@
-#main for command line utility to convert 4 number word file format to 6 number word file format.
+#command line utility to convert 4 number word file format to 6 number word file format.
 from sys import argv
-from parse import parse
+from Parser import Parser
 
 def reformat(memory_list):
     #list_of_commands = [10, 11, 20, 21, 30, 31, 32, 33, 40, 41, 42, 43]
-    j = 0
     for count, i in enumerate(memory_list):
-        #print(count)
         if len(str(i)) == 4:
-            #ordinary old format word
+            #if is ordinary old format word
             i = str(i)
-            i = i[:3] + "0" + i[3:]
+            i = i[:2] + "0" + i[2:]
             i = int(i)
             memory_list[count] = i
         else:
             pass
     return memory_list
+
+def write_file(mem_list_param, argv_num):
+    with open(argv[argv_num], "w") as given_file:
+            for i in mem_list_param:
+                if i == -99999:
+                    given_file.write("-99999")
+                    break
+                elif i != 0:
+                    given_file.write("+0" + str(i) + "\n")
+                else:
+                    # i == 0
+                    given_file.write("+000000" + "\n")
             
 def main():
-
-    memory_list = parse(argv[1])
+    parser_obj = Parser()
+    memory_list = parser_obj.parse(argv[1])
     reformatted_mem_list = reformat(memory_list)
+    for i in reformatted_mem_list:
+        print(i)
     
     if len(argv) == 3:
         #2 files names given
-        with open(argv[2], "w") as given_file:
-            for i in reformatted_mem_list:
-                if i != 0:
-                    given_file.write("+0" + str(i) + "\n")
-                else:
-                    given_file.write("+" + str(i) + "\n")
+        write_file(reformatted_mem_list, 2)
     elif len(argv) == 2:
         #1 file name given
-        with open(argv[1], "w") as given_file:
-            for i in reformatted_mem_list:
-                if i != 0:
-                    given_file.write("+0" + str(i) + "\n")
-                else:
-                    given_file.write("+" + str(i) + "\n")
+        write_file(reformatted_mem_list, 1)
     else:
         print("Error: No file name given.")
 
