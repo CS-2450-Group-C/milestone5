73a66224b7648a33994eba68c5a2c97508538637
dallondmonson <dallondmonson@gmail.com>
2023-04-08 16:15:20 -0600

    Fix existing unit tests (Update to 6-digit)

--- unit_test.py
+++ unit_test.py
@@ -5,92 +5,92 @@ from uvsim import Machine
 from Input import Input
 
 def test_add():
-    memory = [2000, 3001]
+    memory = [20000, 30001]
     machine = Machine(memory)
     while machine.is_running():
         machine.tick()
-    assert machine.debug_get_accumulator() == 5001
+    assert machine.debug_get_accumulator() == 50001
 
 def test_add2():
-    memory = [2000, 3000]
+    memory = [20000, 30000]
     machine = Machine(memory)
     while machine.is_running():
         machine.tick()
-    assert machine.debug_get_accumulator() == 4000
+    assert machine.debug_get_accumulator() == 40000
 
 def test_subtract():
-    memory = [2000, 3001, 3100]
+    memory = [20000, 30001, 31000]
     machine = Machine(memory)
     while machine.is_running():
         machine.tick()
-    assert machine.debug_get_accumulator() == 3001
+    assert machine.debug_get_accumulator() == 30001
 
 def test_subtract2():
-    memory = [2000, 3000, 3100]
+    memory = [20000, 30000, 31000]
     machine = Machine(memory)
     while machine.is_running():
         machine.tick()
-    assert machine.debug_get_accumulator() == 2000
+    assert machine.debug_get_accumulator() == 20000
 
 def test_divide():
-    memory = [2000, 3001, 3200]
+    memory = [20000, 30001, 32000]
     machine = Machine(memory)
     while machine.is_running():
         machine.tick()
     assert machine.debug_get_accumulator() == 2
 
 def test_divide2():
-    memory = [2000, 3000, 3000, 3200]
+    memory = [20000, 30000, 30000, 32000]
     machine = Machine(memory)
     while machine.is_running():
         machine.tick()
     assert machine.debug_get_accumulator() == 3
 
 def test_multiply():
-    memory = [2000, 3001, 3300]
+    memory = [20000, 30001, 33000]
     machine = Machine(memory)
     while machine.is_running():
         machine.tick()
-    assert machine.debug_get_accumulator() == 10002000
+    assert machine.debug_get_accumulator() == 1000020000
 
 def test_multiply2():
-    memory = [2000, 3000, 3301]
+    memory = [20000, 30000, 33001]
     machine = Machine(memory)
     while machine.is_running():
         machine.tick()
-    assert machine.debug_get_accumulator() == 12000000
+    assert machine.debug_get_accumulator() == 1200000000
 
 def test_load():
-    memory = [2000, 3001, 3300, 2002]
+    memory = [20000, 30001, 33000, 20002]
     machine = Machine(memory)
     while machine.is_running():
         machine.tick()
-    assert machine.debug_get_accumulator() == 3300
+    assert machine.debug_get_accumulator() == 33000
 
 def test_load2():
-    memory = [2000, 3301, 3202, 2002]
+    memory = [20000, 33001, 32002, 20002]
     machine = Machine(memory)
     while machine.is_running():
         machine.tick()
-    assert machine.debug_get_accumulator() == 3202
+    assert machine.debug_get_accumulator() == 32002
 
 def test_store():
-    memory = [2000, 3001, 3300, 2000, 2106]
+    memory = [20000, 30001, 33000, 20000, 21006]
     machine = Machine(memory)
     while machine.is_running():
         machine.tick()
-    assert machine._memory[6] == 2000
+    assert machine._memory[6] == 20000
 
 def test_store2():
-    memory = [2000, 3001, 3300, 2002, 2106]
+    memory = [20000, 30001, 33000, 20002, 21006]
     machine = Machine(memory)
     while machine.is_running():
         machine.tick()
-    assert machine._memory[6] == 3300
+    assert machine._memory[6] == 33000
 
 def test_read():
     # Test signed number
-    memory = [1002]
+    memory = [10002]
     machine = Machine(memory)
     with mock.patch.object(builtins, 'input', lambda _: '-1234'):
         while machine.is_running():
@@ -102,7 +102,7 @@ def test_read():
     assert machine._memory[2] == -1234
     
     # Test unsigned number
-    memory = [1002]
+    memory = [10002]
     machine = Machine(memory)
     with mock.patch.object(builtins, 'input', lambda _: '1234'):
         while machine.is_running():
@@ -115,29 +115,30 @@ def test_read():
 
 def test_write(capfd):
     # Test occupied memory
-    memory = [1101, 4300]
+    memory = [11001, 43000]
     machine = Machine(memory)
     while machine.is_running():
         machine.tick()
     out, err = capfd.readouterr()
-    assert out[-6:-1] == '+4300'
+    assert machine.get_memory_at_address(1) == 43000
     # Test empty memory
     memory = [1102, 4300]
     machine = Machine(memory)
     while machine.is_running():
         machine.tick()
     out, err = capfd.readouterr()
-    assert out[-6:-1] == '+0000'
+    
+    assert machine.get_memory_at_address(2) == 0
     
 
 def test_branch():
     # Test branch ahead
-    memory = [4002, 0000, 4300]
+    memory = [40002, 00000, 43000]
     machine = Machine(memory)
     machine.tick()
     assert machine.debug_get_program_counter() == 2
     # Test branch behind
-    memory = [4002, 4300, 4001]
+    memory = [40002, 43000, 40001]
     machine = Machine(memory)
     machine.tick()
     machine.tick()
@@ -145,12 +146,12 @@ def test_branch():
 
 def test_branch_neg():
     # Test non negative
-    memory = [4102, 0000, 4300]
+    memory = [41002, 00000, 43000]
     machine = Machine(memory)
     machine.tick()
     assert machine.debug_get_program_counter() == 1
     # Test negative
-    memory = [4102, 0000, 4300]
+    memory = [41002, 00000, 43000]
     machine = Machine(memory)
     machine.debug_set_accumulator(-1)
     machine.tick()
@@ -158,19 +159,19 @@ def test_branch_neg():
 
 def test_branch_zero():
     # Test non zero
-    memory = [4202, 0000, 4300]
+    memory = [42002, 00000, 43000]
     machine = Machine(memory)
     machine.debug_set_accumulator(1)
     machine.tick()
     assert machine.debug_get_program_counter() == 1
     # Test zero
-    memory = [4202, 0000, 4300]
+    memory = [42002, 00000, 43000]
     machine = Machine(memory)
     machine.tick()
     assert machine.debug_get_program_counter() == 2
 
 def test_halt():
-    memory = [4300]
+    memory = [43000]
     machine = Machine(memory)
     count = 0
     while machine.is_running():
